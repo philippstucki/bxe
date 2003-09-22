22ed03cbead984859288453fce4081c43d00d0e6
chregu <chregu@7e49d481-a7d8-0310-bbc4-96d0c2794ec3>
2003-09-22 16:11:11 +0000

    remember choices
    git-svn-id: https://svn.liip.ch/repos/bxe/trunk@271 7e49d481-a7d8-0310-bbc4-96d0c2794ec3

--- relaxng/AttributeVDOM.js
+++ relaxng/AttributeVDOM.js
@@ -1,11 +1,20 @@
 function AttributeVDOM(node) {
 	this.name = node.getAttribute("name");
 	this.dataType = "NCName";
-	//dump(node.getAttribute("name") + ": " + node.childNodes.length + "\n");
 	for (var i = 0; i < node.childNodes.length;i++  ) {
 		//dump(i + node.childNodes[i].nodeName );
 		if (node.childNodes[i].nodeName == "data") {
 			this.dataType = node.childNodes[i].getAttribute("type");
+		}
+		else if (node.childNodes[i].nodeName == "choice") {
+			this.dataType = "choice";
+			this.choices = new Array();
+			var choice = node.childNodes[i].childNodes;
+			for (var j = 0; j < choice.length; j++) {
+				if (choice[j].localName == "value" && choice[j].firstChild) {
+				this.choices.push(choice[j].firstChild.data);
+				}
+			}
 		}
 		
 	}
