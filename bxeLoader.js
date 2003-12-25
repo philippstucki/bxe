aefd69d229cd21651e1784e8b6e8343c475827f3
chregu <chregu@7e49d481-a7d8-0310-bbc4-96d0c2794ec3>
2003-12-25 02:30:08 +0000

    automatic BuildDate Update
    git-svn-id: https://svn.liip.ch/repos/bxe/trunk@478 7e49d481-a7d8-0310-bbc4-96d0c2794ec3

--- bxeLoader.js
+++ bxeLoader.js
@@ -11,10 +11,10 @@
 // | Author: Christian Stocker <chregu@bitflux.ch>                        |
 // +----------------------------------------------------------------------+
 //
-// $Id: bxeLoader.js,v 1.165 2003/12/24 02:30:07 chregu Exp $
+// $Id: bxeLoader.js,v 1.166 2003/12/25 02:30:08 chregu Exp $
 
 const BXE_VERSION = "0.1alpha";
-const BXE_BUILD = "200312240330"
+const BXE_BUILD = "200312250330"
 
 const E_FATAL = 1;
 
