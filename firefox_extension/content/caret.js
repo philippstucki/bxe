9f80dcf349fad513053049f6984a07793fd01bb3
chregu <chregu@7e49d481-a7d8-0310-bbc4-96d0c2794ec3>
2005-06-07 13:17:47 +0000

    more general caret setting
    git-svn-id: https://svn.liip.ch/repos/bxe/trunk@1336 7e49d481-a7d8-0310-bbc4-96d0c2794ec3

--- firefox_extension/content/caret.js
+++ firefox_extension/content/caret.js
@@ -1,25 +1,33 @@
 document.addEventListener("click", bxehelper_mouse, false);
 
+
 function bxehelper_mouse(e) {
-	var target = e.target.parentElement;
-	if(target && target.userModifiable ) {
-		if(e.target.tagName=="tabbrowser") {
-			return;
-		}
-		// if the target of the click is one of these, then don't change the status of the toolbar
-		var protect = new Array( 'toolbar', 'toolbarbutton', 'menuitem', 'menu', 'colorpicker');
-		for(var i=0; i<protect.length; i++) {
-			if(e.target.tagName==protect[i]) {
+	try {
+	if (e.target.ownerDocument.defaultView.BXENS) {
+		var target = e.target.parentNode;
+		if(target && target.userModifiable ) {
+			if(e.target.tagName=="tabbrowser") {
 				return;
 			}
+			// if the target of the click is one of these, then don't change the status of the toolbar
+			var protect = new Array( 'toolbar', 'toolbarbutton', 'menuitem', 'menu', 'colorpicker');
+			for(var i=0; i<protect.length; i++) {
+				if(e.target.tagName==protect[i]) {
+					return;
+				}
+			}
+			
+			
+			bxehelper_setCaret();
 		}
-		
-		
-		bxehelper_setCaret();
 	} else {
 		bxehelper_removeCaret();
 	}
-
+	} catch (e) {
+		dump("bxehelper catched\n");
+	}
+	
+	
 }
 
 function bxehelper_removeCaret() {
